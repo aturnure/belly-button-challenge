b1401e29b5f9bfbf73d11aaab9598f535f760f8c
aturnure <aturnure@gmail.com>
2024-04-04 21:38:37 -0700

    Updated code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,66 +1,132 @@
 // Use the D3 library to read in samples.json from the URL
 const samplesURL = "https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json";
 
-// Initializes the page with a default plot
-function init() {
-
-  // Use d3 to select the dropdown menu
-  let dropdownMenu = d3.select("#selDataset");
+// Function to collect the data from the metadata list in the samples json 
+function buildMetadata(sample) {
 
   // Fetch the json data with d3
   d3.json(samplesURL).then(function(data) {
-    console.log(data);
-
-    // Put Sample IDs in the dropdown menu
-    data.names.forEach(function(sampleID) {
-      dropdownMenu.append("option").text(sampleID).property("value", sampleID);
+    let sampleData = data.metadata;
+    let resultList = sampleData.filter(function(metadata) {
+      return metadata.id == sample;
     });
 
-    // Make first sample default data for chart
-    createBarChart(data.names[0], data);
+    let IDdata = d3.select("#sample-metadata");
 
-    // Prepare dropdown for change
-    dropdownMenu.on("change", function() {
-      let selectedId = dropdownMenu.property("value");
-      createBarChart(selectedId, data);
+    // Clear previous content
+    IDdata.html("");
+
+    // Loop to include the demographic info in the list on the side for each id selected
+    Object.entries(resultList[0]).forEach(function(pair) {
+      let key = pair[0];
+      let value = pair[1];
+
+      let h6data = IDdata.append("h6");
+      h6data.text(`${key}: ${value}`);
     });
   });
 }
 
-// Create bar chart
-function createBarChart(sample_id, data) {
+// Create bar and bubble charts
+// Call function to build charts
+function buildCharts(sample) {
+  d3.json(samplesURL).then(function(data) {
+    let samples = data.samples;
+    let sampList = samples.filter(function(samples) {
+      return samples.id == sample;
+    });
+    let results = sampList[0];
 
-  // Sort sample_values by descending
-  let sample = data.samples.find(sample => sample.id == sample_id);
-  let sortedByValue = sample.sample_values.slice().sort((a, b) => b - a);
+    // Get desired data for bar chart
+    let bar_x = results.sample_values.slice(0, 10).reverse();
+    let bar_y = results.otu_ids.slice(0, 10).map(function(otuid) {
+      return "OTU" + otuid;
+    }).reverse();
+    let bar_text = results.otu_labels.slice(0, 10).reverse();
 
-  // Slice the first 10 objects for plotting
-  let slicedSampValue = sortedByValue.slice(0, 10);
+    // Get desired data for bubble chart
+    let bubble_x = results.otu_ids;
+    let bubble_y = results.sample_values;
+    let bubble_text = results.otu_labels;
 
-  // Reverse the array to accommodate Plotly's defaults
-  slicedSampValue.reverse();
+    // Create bar chart
+    let trace1 = {
+      y: bar_y,
+      x: bar_x,
+      text: bar_text,
+      type: "bar",
+      orientation: "h",
+    };
 
-  // Trace for data
-  let traceSamp = {
-    x: slicedSampValue.map(object => object.sample_value),
-    y: slicedSampValue.map(object => `OTU ${object.otu_id}`),
-    text: slicedSampValue.map(object => object.otu_label),
-    name: "OTU Chart",
-    type: "bar",
-    orientation: "h"
-  };
+    // Data array
+    let data1 = [trace1];
 
-  // Data array
-  let otuData = [traceSamp];
+    // Create layout
+    let layout1 = {
+      title: "Top 10 Bacteria Found Per Individual",
+      margin: {
+        l: 100,
+        t: 40
+      }
+    };
 
-  // Apply title
-  let layout1 = {
-    title: "Top 10 OTU's per Individual",
-  };
+    // Render the plot to the div tag with the id "bar"
+    Plotly.newPlot("bar", data1, layout1);
 
-  // Render the plot
-  Plotly.newPlot("bar", otuData, layout1);
+    // Create bubble chart
+    let trace2 = {
+      x: bubble_x,
+      y: bubble_y,
+      text: bubble_text,
+      mode: "markers",
+      marker: {
+        size: bubble_y,
+        color: bubble_x
+      }
+    };
+    
+    // Data array
+    let data2 = [trace2];
+  
+    // Create layout
+    let layout2 = {
+      title: "All Bacteria Present per Sample",
+      margin: {t: 0},
+      hovermode: "closest",
+      xaxis: {title: "OTU ID"},
+      margin: {t: 25}
+    };
+
+    // Render the plot to the div tag with the id "bar"
+    Plotly.newPlot("bubble", data2, layout2);
+  });
 }
 
-init();
+// Function to append dropdown menu and have a default chart upon opening
+function init() {
 
+  // Use d3 to select the dropdown menu
+  let dropdown = d3.select("#selDataset");
+
+  // Use names list in data to get all the sample names
+  d3.json(samplesURL).then(function(data) {
+    let sample_names = data.names;
+
+    for (let i = 0; i < sample_names.length; i++) {
+      dropdown.append("option").text(sample_names[i]).property("value", sample_names[i]);
+    };
+
+    // Use the first sample as the default when opening the page
+    let default_sample = sample_names[0];
+    buildCharts(default_sample);
+    buildMetadata(default_sample);
+  });
+}
+
+// Create a function that will render the new data charts for each selected data point
+function optionChanged(new_selection) {
+  buildCharts(new_selection);
+  buildMetadata(new_selection);
+}
+// Initialize 
+init()
